c2bd3b6a86eede9db35027df992ad85c4ea5dd46
Uri Ronen <ronenuri@gmail.com>
2018-05-01 15:06:59 +0300

    first commit

---
+++ ImageServiceGUI/LogModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGUI
+{
+    public class LogModel : ILogModel
+    {
+        public void GetLog()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            TcpClient client = new TcpClient();
+            client.Connect(ep);
+
+            using (NetworkStream stream = client.GetStream())
+            using (BinaryReader reader = new BinaryReader(stream))
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write("GetLogIN ENUM TYPE");
+                reader.Read();
+            }
+        }
+    }
+}
---
+++ ImageServiceGUI/SettingsModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGUI
+{
+    class SettingsModel : ISettingsModel
+    {
+        public void GetConfig()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            TcpClient client = new TcpClient();
+            client.Connect(ep);
+
+            using (NetworkStream stream = client.GetStream())
+            using (BinaryReader reader = new BinaryReader(stream))
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write("GetConfigIN ENUM TYPE");
+                reader.Read();
+
+            }
+            client.Close();
+        }
+
+        public void RemoveHandler(string handlerPath)
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            TcpClient client = new TcpClient();
+            client.Connect(ep);
+
+            using (NetworkStream stream = client.GetStream())
+            using (BinaryReader reader = new BinaryReader(stream))
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write(handlerPath.ToArray(), 0, handlerPath.Length);
+                bool res = reader.ReadBoolean();
+            }
+            client.Close();
+        }
+    }
+}
