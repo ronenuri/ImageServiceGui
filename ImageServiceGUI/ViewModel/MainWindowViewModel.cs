a3e68a924020422952cc68586b621265be01640e
Uri Ronen <ronenuri@gmail.com>
2018-05-17 09:26:13 +0300

    Added mainwindow view model and model

---
+++ ImageServiceGUI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ImageServiceGUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGUI.ViewModel
+{
+    class MainWindowViewModel
+    {
+        IMainWindowViewModel mainWindowModel;
+
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
